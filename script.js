5865c512dad91a699546cbd674ad4e783dc3e25a
Sara2708 <sara27082011@gmail.com>
2021-05-03 19:47:25 -0300

    add movimento à cobrinha

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ snake[0] = {
     x: 8 * box,
     y: 8* box
 }
+let direction = "right";
 
 function criarBG(){
     context.fillStyle = "lightgreen"; // fillStyle é o estilo do fundo, a cor
@@ -20,5 +21,27 @@ function criarCobrinha(){
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+
