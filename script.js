fe805335f1beeff6620c72dd372de2ee27803f70
Sara2708 <sara27082011@gmail.com>
2021-05-03 21:12:43 -0300

    add game over

--- script.js
+++ script.js
@@ -40,11 +40,18 @@ function update(event){//direção que a cobrinha anda, começando pela direita,
 }
 
 function iniciarJogo(){
+
     //condições para a cobrinha reaparecer do outro lado quando bater em uma borda
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;//quando bate do limite de quadradinhos do context.fillRect, ela começa de novo no quadradinho 0
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            alert("Game Over :(");
+        }
+    }
 
     criarBG();//chamou as funções de cima dentro dessa função para quando iniciar o jogo estar tudo ok
     criarCobrinha();
@@ -53,19 +60,19 @@ function iniciarJogo(){
     let snakeX = snake[0].x;//variáveis da direção que a cobrinha anda no plano cardeal
     let snakeY = snake[0].y;
 
-    if(direction == "right") snakeX += box;//se anda para direita, acrescenta um quadradinho verde á direita - a mesma lógica para os de baixo - no plano cardeal
+    if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
-    if(direction == "up") snakeY -= box;
+    if (direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    if(snakeX != food.x || snake.Y != food.y){
+    if(snakeX != food.x || snakeY != food.y){
         snake.pop();//retira o ultimo elemento da array snake, para não ficar o quadradinho verde atrás, onde a cobrinha já passou
     }else{
-        food.x = Math.floor(Math.random() * 15 + 1) * box;
-        food.y = Math.floor(Math.random() * 15 + 1) * box;
+        food.x = Math.floor(Math.random() * 15 +1) * box;
+        food.y = Math.floor(Math.random() * 15 +1) * box;
     }//continuar aparecendo comidinha depois q a cobrinha comer
 
-    
+
     
     let newHead = {
         x: snakeX,
